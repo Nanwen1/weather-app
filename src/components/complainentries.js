356ebff179c70353c3b9287260555d1d5aa3ccbd
Nanwen <nanwens@gmail.com>
2019-11-24 13:30:00 +0800

    form

--- src/components/complainentries.js
+++ src/components/complainentries.js
@@ -3,21 +3,47 @@ import { MiniTitle} from '../elements/minititle';
 import { Field } from '../elements/entryfields';
 import { Button } from '../elements/button';
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+
+
 export function ComplainForm(props) {
+
+    const [name, setName] = React.useState('')
+    const [email, setEmail] = React.useState('')
+    const [complaint, setComplaint] = React.useState('')
+
+
+    const handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
     return (
         <div className="hourDisplayClass">
-          <form name="complaint-form" method="POST" data-netlify="true">
+          <form name="complaint-form" method="POST" data-netlify="true" onSubmit={handleSubmit}>
             <div>
               <MiniTitle location="Your name"/>
-              <Field name="name" type="text" />
+              <Field name="name" type="text" set={setName} value={name} />
             </div>
             <div>
               <MiniTitle location="Email"/>
-              <Field name="email" type="email" />
+              <Field name="email" type="email" set={setEmail} value={email} />
             </div>
             <div>
-              <MiniTitle location="Complaint"/>
-              <Field name="complain" type="textarea" />
+              <MiniTitle location="Complaint" />
+              <Field name="complain" type="textarea" set={setComplaint} value={complaint} />
             </div>
             <Button/>
           </form>
--- src/elements/entryfields.js
+++ src/elements/entryfields.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 export function Field(props) {
     
-    const { name, type } = props
+    const { name, type, set, value } = props
+
+    const handleChange = (e) => {
+      const value = e.target.value
+      set(value)
+    }
 
     if (type === 'textarea') {
       return (
-        <textarea className="entryFieldClass" name={name}></textarea>
+        <textarea className="entryFieldClass" name={name} value={value} onChange={handleChange} />
       )
     }
 
     return (
-      <input className="entryFieldClass" type={type} name={name} />
+      <input className="entryFieldClass" type={type} name={name} value={value} onChange={handleChange} />
     )
   }
